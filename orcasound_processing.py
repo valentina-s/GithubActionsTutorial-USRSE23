d2fc3fc0e4a218f9e42141eb26cb54a5b315d409
Valentina Staneva <valentina-s@users.noreply.github.com>
2023-10-01 23:35:15 -0700

    Last ts file

--- orcasound_processing.py
+++ orcasound_processing.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
 
     convert2wav(path.normpath(args.input_dir), "wav")
 
-    for input_wav in sorted(glob.glob("wav/*.wav"))[:1]:
+    for input_wav in sorted(glob.glob("wav/*.wav"))[-1:]:
         # output_fname = create_spec_name(input_wav, args.output)
         output_fname = path.join(args.input_dir, "spec.png")
         save_spectrogram(input_wav, output_fname, args.nfft)
